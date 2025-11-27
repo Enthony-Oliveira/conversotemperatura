027ffbe34fc2baa76be8c64855b5d404c7a28d7d
Enthony <oliveiraenthony987@gmail.com>
2025-11-27 18:03:23 -0300

    botao retornando nulo

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
 
 export default function App() {
+
+  const [valor, setValor] = useState('');
+  const [resultado, setResultado] = useState(null);
+
+  function conversor() {
+
+    const numero = Number(valor);
+
+    if (valor.trim === '') {
+      Alert.alert("Digite um valor valido");
+      return;
+
+    }
+
+    const fahrenheit = (numero * 1.8) + 32;
+
+    Alert.alert(
+      "Conversão Realizada",
+      `O Resultado é ${fahrenheit.toFixed(2)} °F`,
+      [
+        {
+          text: "Ok",
+          onPress: () => {
+            setValor('');
+            setResultado(null);
+          }
+        }
+      ]
+    );
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.appbar}>
+        <Text style={styles.tittle}>Conversor de Temperatura</Text>
+      </View>
+
+      <View style={styles.content}>
+        <Text style={styles.titleconteudo} >Digite a Temperatura</Text>
+        <TextInput
+          placeholder='Digite Aqui'
+          style={styles.conteudo}
+          value={valor}
+          onChangeText={setValor}
+          keyboardType='numeric'>
+
+        </TextInput>
+
+        <TouchableOpacity style={styles.btn} onPress={conversor}>
+          <Text style={styles.btntext}>Converter</Text>
+        </TouchableOpacity>
+
+        {resultado && (<Text style={styles.textresultado}>{resultado}</Text>)}
+
+      </View>
+
     </View>
   );
 }
@@ -13,8 +66,61 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+
+  appbar: {
+    height: 70,
+    backgroundColor: '#3B82F6',
+    justifyContent: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    elevation: 4
+  },
+
+  tittle: {
+    color: '#fff',
+    textAlign: 'center',
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  titleconteudo: {
+    fontSize: 20,
+    fontFamily: 'sans-serif',
+    fontWeight: 'bold',
+    color: '#000',
+  },
+
+  conteudo: {
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 20,
+    fontSize: 16,
+    margin: 10,
+    width: '80%',
+  },
+
+  btn: {
+    backgroundColor: '#3B82F6',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginTop: 20,
     alignItems: 'center',
     justifyContent: 'center',
+    width: 200,
   },
+
+  btntext: {
+    fontFamily: 'sans-serif',
+    fontSize: 16,
+    color: '#fff'
+  }
 });
